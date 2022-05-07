574e6578cf96ad2d0e1d08a3667ac74e2773a316
Astuti <100201094+astutirahmawati@users.noreply.github.com>
2022-05-07 14:04:00 +0700

    skeleton

---
+++ config/database.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"fmt"
+	"together/be8/entities"
+
+	"github.com/labstack/gommon/log"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func InitDB(config AppConfig) *gorm.DB {
+	fmt.Printf("%v", config)
+	conString := fmt.Sprintf("%s:@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True",
+		config.User,
+		config.Host,
+		config.DBPort,
+		config.DBName,
+	)
+
+	db, err := gorm.Open(mysql.Open(conString), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	return db
+}
+func Migrate(db *gorm.DB) {
+	db.AutoMigrate(&entities.User{})
+}
---
+++ entities/user.go
@@ -0,0 +1,15 @@
+package entities
+
+import (
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `gorm:"unique" json:"password" form:"password"`
+	// Address  []Address `gorm:"foreignKey:Owner"`
+	Phone  string `json:"phone"`
+	Status string `json:"status"`
+}
